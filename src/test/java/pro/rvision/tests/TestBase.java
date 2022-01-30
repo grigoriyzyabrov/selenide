8cf500836f07f74637929cf541ba2d3fa4972bcb
gzyabrov <gzyabrov@rvision.pro>
2022-01-30 17:17:41 +0300

    add selenoid

--- src/test/java/pro/rvision/tests/TestBase.java
+++ src/test/java/pro/rvision/tests/TestBase.java
@@ -2,6 +2,7 @@ package pro.rvision.tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pro.rvision.pages.RegistrationPage;
 
 public class TestBase {
@@ -12,5 +13,11 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 }
