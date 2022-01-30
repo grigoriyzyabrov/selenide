f5959195b28394da3b36dba1bde4271b7de9e092
gzyabrov <gzyabrov@rvision.pro>
2022-01-30 17:28:40 +0300

    add allure video

--- src/test/java/pro/rvision/tests/TestBase.java
+++ src/test/java/pro/rvision/tests/TestBase.java
@@ -1,15 +1,23 @@
 package pro.rvision.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import pro.rvision.helpers.Attach;
 import pro.rvision.pages.RegistrationPage;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
     static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
@@ -19,5 +27,14 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 }
