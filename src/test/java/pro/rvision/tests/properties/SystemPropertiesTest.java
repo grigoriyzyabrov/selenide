7dbaee026bb2be256285b28e0fa761958d583bb7
gzyabrov <gzyabrov@rvision.pro>
2022-02-06 17:08:33 +0300

    add system properties test

--- src/test/java/pro/rvision/tests/properties/SystemPropertiesTest.java
+++ src/test/java/pro/rvision/tests/properties/SystemPropertiesTest.java
@@ -28,4 +28,16 @@ public class SystemPropertiesTest {
         String browser = System.getProperty("browser");
         System.out.println(browser);
     }
+
+    @Test
+    @Tag("properties")
+    void someTest5(){
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String browserSize = System.getProperty("browserSize", "300x300");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+    }
 }
